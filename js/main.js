ee0f208e97f3bc79e321d744ca7c4bc6b55bdd57
Fariol Blondeau <blondeau.nbif@mail.ru>
2023-04-11 11:23:47 +0300

    updating

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-import routes from "./routes.js";
+const pageTitle = "JavaScript SPA";
+
+const routes = {
+  404: {
+    template: "../pages/404.html",
+    title: `Page not found | ${pageTitle}`,
+    description: "This is only the missing page",
+  },
+  "/": {
+    template: "../pages/home.html",
+    title: `Home page | ${pageTitle}`,
+    description: "Home page",
+  },
+  "/about": {
+    template: "../pages/about.html",
+    title: `About page | ${pageTitle}`,
+    description: "About page",
+  },
+  "/contact": {
+    template: "../pages/contact.html",
+    title: `contact page | ${pageTitle}`,
+    description: "Conatact page",
+  },
+};
 
 document.addEventListener("click", (e) => {
   const { target } = e;
